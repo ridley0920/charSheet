b326ade9844881674de6bb8611b32e9713ae2ca4
Ryan Counts <Ryan Counts>
2016-10-21 12:22:44 -0400

    continued work

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ var
   express         = require('express'),
   morgan          = require('morgan'),
   MongoClient     = require('mongodb').MongoClient, 
-  assert          = require('assert');
+  assert          = require('assert'),
+  fs              = require('fs');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/myproject';
@@ -22,27 +23,28 @@ MongoClient.connect(url, function(err, db) {
 
 var app = module.exports = express();
 var server = http.Server(app);
-var io = require('socket.io')(server);
-var fs = require('fs');
+var io = socket(server);
 
-server.listen(80);
+
+//server.listen(80);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile('F:/Programming/charSheet/front/index.jade');
 });
 
-io.on('connection', function (socket) {
+/*io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
-});
+});*/
 
-app.use('/h1', require('../server/routes/chars.js'));
-app.use('/', require('../server/routes/defaults'));
+/*app.use('/h1', require('../server/routes/chars.js'));
+app.use('/', require('../server/routes/defaults'));*/
 
 if(!module.parent)
 {
+  //app.listen(3001);
   server.listen(3001);
   console.log('Dev Server started on port 3001');
 }
